39b815c8aa6d1bacd658a591e45b7c1a7026c7a1
Murilo Silva <contato@silvamurilo.com.br>
2025-05-05 22:03:25 -0300

    Feat: tipos de variáveis

---
+++ variaveis.php
@@ -0,0 +1,18 @@
+<?php
+    // STRING: armazena textos, como palavras, frases ou qualquer sequência de caracteres.
+    $texto = 'Olá, Mundo!';
+
+    // NUMBER (inteiro ou decimal): usado para armazenar valores numéricos, como idade, preço, etc.
+    $numeroInteiro = 23;
+    $numeroDecimal = 99.50;
+
+    // BOOLEAN: representa valores lógicos — verdadeiro (true) ou falso (false).
+    $usuarioAtivo = true;
+
+    // ARRAY: usado para armazenar uma coleção de valores, que podem ser acessados por índices ou chaves.
+    $usuario = [
+        "nome" => "Maria",
+        "idade" => 30,
+        "ativo" => true
+    ];
+?>
